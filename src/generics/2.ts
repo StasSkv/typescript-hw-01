fe7b5ee653853120ebe5cdc549ea061a7f1b396e
StasSkv <stas000123@gmail.com>
2025-05-18 23:08:21 +0300

    ok

---
+++ src/generics/2.ts
@@ -0,0 +1,21 @@
+type AllType = {
+  name: string;
+  position: number;
+  color: string;
+  weight: number;
+};
+
+type TopType = Pick<AllType, "name" | "color">;
+type BottomType = Pick<AllType, "position" | "weight">;
+
+function compare<T extends TopType, B extends BottomType>(
+  top: T,
+  bottom: B
+): AllType {
+  return {
+    name: top.name,
+    color: top.color,
+    position: bottom.position,
+    weight: bottom.weight,
+  };
+}
