b42b413bb987c64746fb854b3ba5e8cc145a79eb
StasSkv <stas000123@gmail.com>
2025-05-18 23:17:42 +0300

    okk

--- src/basic/7.ts
+++ src/basic/7.ts
@@ -9,18 +9,18 @@ type Page = {
   };
 };
 
-const page1 = {
+const page1: Page = {
   title: "The awesome page",
   likes: 100,
   accounts: ["Max", "Anton", "Nikita"],
   status: "open",
   details: {
-    createAt: new Date("2021-01-01"),
+    cerateAt: new Date("2021-01-01"),
     updateAt: new Date("2021-05-01"),
   },
 };
 
-const page2 = {
+const page2: Page = {
   title: "Python or Js",
   likes: 5,
   accounts: ["Alex"],
